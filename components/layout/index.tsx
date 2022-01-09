b105cf08ada7b6396ea834434bed630ce0a857a7
quavious <leenowon97@gmail.com>
2022-01-10 01:40:43 +0900

    Feat: Adsense Script Tag

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,15 +1,25 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 const Layout = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return (
-    <div className="pt-6 px-2">
-      <div className="flex flex-col container w-96 mx-auto">
-        <h1 className="text-4xl font-bold mx-auto text-white cursor-pointer">
-          <Link href={'/'}>Rhyme Times!</Link>
-        </h1>
-        {children}
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8567722160341015"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <div className="pt-6 px-2">
+        <div className="flex flex-col container w-96 mx-auto">
+          <h1 className="text-4xl font-bold mx-auto text-white cursor-pointer">
+            <Link href={'/'}>Rhyme Times!</Link>
+          </h1>
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
